0aa9d0049c755b0995d9aa532e2607ae16700dce
kmeans00 <minsu00222@gmail.com>
2024-09-26 11:13:01 +0900

    modify test03.py by user01

--- test03.py
+++ test03.py
@@ -1,5 +1,7 @@
 def sayHello(name):
-    return 'Hello, ' + name
+    return 'Hello, ' + name + '!!' # user01 수정
 
-inName = Tom
+inName = Tom 
 print(sayHello(inName))
+
+print('Requestd by boss') # user01 추가
